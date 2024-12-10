5f67f5b3307a1d711fb003fd6f3e327b753a854b
hgOscNor <oscar.noring@hitachigymnasiet.se>
2024-12-10 10:37:48 +0100

    added base js for firebase

---
+++ webbsite/app.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyC3sxf274LVPT_RTw-I5TS4N6c3YbaMql4",
+    authDomain: "wexteras-8d3c3.firebaseapp.com",
+    projectId: "wexteras-8d3c3",
+    storageBucket: "wexteras-8d3c3.firebasestorage.app",
+    messagingSenderId: "896283578937",
+    appId: "1:896283578937:web:cc315228718995ae7273e8"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
