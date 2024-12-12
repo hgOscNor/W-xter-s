ed82403e0bbd0cac4c0ee95ea9f6eeebe53a8b9b
hgOscNor <oscar.noring@hitachigymnasiet.se>
2024-12-12 15:49:19 +0100

    pushing and reading exampledata to and from firebase

---
+++ esp/wexterasJordLuft/jordLuft/secrets.h
@@ -0,0 +1,10 @@
+
+//School wifi login
+#define WIFI_SSID     "Hitachigymnasiet_2.4"
+#define WIFI_PASSWORD "mittwifiarsabra"
+
+// Database url
+#define REFERENCE_URL "https://wexteras-8d3c3-default-rtdb.europe-west1.firebasedatabase.app/"
+
+/* Uncomment the following line for Locked Mode (With Authentication) */
+#define AUTH_TOKEN "wgCdF4hVqFJRKiwyJNnwO5DjsRQngRKHLCVu68V8"
