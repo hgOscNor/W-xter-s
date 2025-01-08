9849f91b30c8af441a7c19ff130063268ccfa04c
hgludrum <ludvig.rummelhoff@hitachigymnasiet.se>
2025-01-08 16:08:26 +0100

    Icke stylad ej firebase fungerande controller

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
-  <router-view />
+  <div class="controllerObj">
+    <ControllerComp name="Fläktkontroller"></ControllerComp>
+    <ControllerComp name="Pump"></ControllerComp>
+    <ControllerComp name="tefghjcvfghst"></ControllerComp>
+  </div>
 </template>
 
 <script setup>
+import ControllerComp from './components/ControllerComp.vue'
 //
 </script>
+
+<style>
+.controllerObj {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  flex-wrap: nowrap;
+}
+</style>
