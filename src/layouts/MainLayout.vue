b51df638600af99b4a35bcf10de6a7b88f6420aa
hglucsil <lucas.fredin.silversten@hitachigymnasiet.se>
2025-01-23 11:25:24 +0100

    Klar med Header för idag

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,16 +1,26 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <!-- HEADER -->
-    <q-header elevated class="bg-primary text-white">
+    <q-header elevated class="bg-green text-white">
       <div class="q-pa-md">
-        <h3>Wäxterås</h3>
-        <q-tabs v-model="tab" class="text-white" align="center" dense>
+        <q-tabs v-model="tab" class="text-white" align="center" style="height: 55px">
           <q-tab
             name="Hem"
             icon="img:https://www.hitachigymnasiet.se/wp-content/uploads/2023/09/logoMark.svg"
-            label="Hem"/>
-          <q-tab name="Manual" icon="alarm" label="Manual"/>
-          <q-tab name="Om oss" icon="movie" label="Om oss"/>
+            label="Hem"
+            style="padding: 0px 20px; font-size: 12px;"/>
+          <q-tab 
+            name="Manual" 
+            icon="alarm" 
+            label="Manual"
+            style="padding: 0px 20px; font-size: 12px;"
+          />
+          <q-tab 
+            name="Om oss" 
+            icon="movie" 
+            label="Om oss"
+            style="padding: 0px 20px; font-size: 12px;"
+            />
         </q-tabs>
       </div>
     </q-header>
